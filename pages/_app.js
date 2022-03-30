0b6c6258d3b3504a6f734e11df90ac58db162d08
vu <tuanvundvn@gmail.com>
2022-03-30 12:15:24 +0700

    change icon

--- pages/_app.js
+++ pages/_app.js
@@ -44,6 +44,7 @@ function MyApp({ Component, pageProps }) {
       <Layout>
         <Head>
           <title>{Component.title ? Component.title + ' | Job Connection' : 'Job Connection'}</title>
+          <link rel="icon" href="/logo.png" />
         </Head>
         <Component {...pageProps} />
       </Layout>
